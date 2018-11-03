2c6cad71a92eb005141f5cacb822286b9999c098
jacksonbrajer <jacksonbrajer@192.168.2.70>
2018-11-03 17:30:10 -0400

    Added a test TRC

--- src/auto/AutoPaths.java
+++ src/auto/AutoPaths.java
@@ -6,7 +6,18 @@ import math.Point;
 
 public class AutoPaths {
 
-	private static AutoPath centerStartLeftSwitch = new AutoPath(Constants.rc, false, null, new Point(0,0,0), new Point(3.3, -1.12912, 0));
+	private static AutoPath centerStartLeftSwitch = new AutoPath(Constants.rc, false, new TimeRangedCommand[] {new TimeRangedCommand(2,2.5, new SingleCommand() {
+		
+		@Override
+		public void run() {
+			System.out.println("STARTED");
+		}
+		
+		@Override
+		public void onEnd() {
+			System.out.println("ENDED");
+		}
+	})}, new Point(0,0,0), new Point(3.3, -1.12912, 0));
 	
 	public static AutoPath getCenterStartLeftSwitch() {
 		return centerStartLeftSwitch;
